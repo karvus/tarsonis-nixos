ff6ebcc8ec6220afd02c739f2caa762f89546ce6
Thomas Stenhaug <thomas@srcs.no>
2025-05-27 21:06:53 +0000

    Make nixvim import work

--- flake.nix
+++ flake.nix
@@ -7,23 +7,25 @@
     nixvim.url = "github:nix-community/nixvim";
   };
 
-  outputs = { self, nixpkgs, home-manager, nixvim, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, nixvim, ... }: {
     nixosConfigurations.tarsonis-vm = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";  # or "aarch64-linux" if you're on ARM
+      system = "aarch64-linux";
 
       modules = [
         ./configuration.nix
-        ./hardware-configuration.nix
         home-manager.nixosModules.home-manager
         {
-          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = {
+            inherit inputs;
+          };
           home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
           home-manager.users.thomas = import ./home.nix;
         }
       ];
 
       specialArgs = {
-        inherit nixvim;
+        inherit inputs;
       };
     };
   };
--- home.nix
+++ home.nix
@@ -1,62 +1,9 @@
-{ config, pkgs, nixvim, ... }:
-
+{ config, pkgs, nixvim, inputs, ... }:
 {
-  imports = [
-    nixvim.homeManagerModules.nixvim
-  ];
+  imports = [ inputs.nixvim.homeManagerModules.nixvim ];
 
   home.username = "thomas";
   home.homeDirectory = "/home/thomas";
   home.stateVersion = "24.11";
 
-  programs.nixvim = {
-    enable = true;
-
-    colorschemes.catppuccin = {
-      enable = true;
-      flavor = "mocha";
-    };
-
-    plugins = {
-      lsp.enable = true;
-      lsp.servers.lua-ls.enable = true;
-      telescope.enable = true;
-      treesitter.enable = true;
-      cmp.enable = true;
-      cmp.sources = [
-        { name = "nvim_lsp"; }
-        { name = "buffer"; }
-      ];
-      luasnip.enable = true;
-      gitsigns.enable = true;
-      which-key.enable = true;
-    };
-
-    options = {
-      number = true;
-      relativenumber = true;
-      expandtab = true;
-      shiftwidth = 2;
-      tabstop = 2;
-    };
-
-    globals.mapleader = " ";
-
-    keymaps = [
-      {
-        mode = "n";
-        key = "<leader>ff";
-        action = "<cmd>Telescope find_files<cr>";
-        options.desc = "Find files";
-      }
-    ];
-  };
-
-  home.packages = with pkgs; [
-    lua-language-server
-    ripgrep
-    fd
-    git
-    fzf
-  ];
 }
