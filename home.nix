3230757e4ca16b41252b8dde15b2e8b4a1aea355
Thomas Stenhaug <thomas.stenhaug@gmail.com>
2025-05-27 14:02:22 +0200

    elaborate zsh config

--- home.nix
+++ home.nix
@@ -4,6 +4,23 @@
   home.homeDirectory = "/home/thomas";
   programs.home-manager.enable = true;
   programs.neovim.enable = true;
-  programs.zsh.enable = true;
+  programs.zsh = {
+    enable = true;
+    enableCompletions = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch /etc/nixos/#tarsonis-vm";
+    };
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-autosuggestions" }
+        { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
+      ];
+    };
+    history.size = 10000;
+  };
   home.stateVersion = "25.05";
 } 
